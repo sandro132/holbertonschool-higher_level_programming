c567beaa86978d7517284ffd3a7a28c2095e4899
sandro132 <mianveo@hotmail.com>
2023-06-05 17:30:17 -0500

    :seedling: add function that removes all characters c and C from a string.

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 def no_c(my_string):
-    n_string = ""
-    for i in my_string:
-        if i != 'c' and i != 'C':
-            n_string += i
-    return n_string
+    new_string = ""
+    for char in my_string:
+        if char not in ['c', 'C']:
+            new_string += char
+    return new_string
