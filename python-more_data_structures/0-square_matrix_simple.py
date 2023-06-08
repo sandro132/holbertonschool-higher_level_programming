106eeccbdca44548e513bd06f0ef18d57b6cdbce
sandro132 <mianveo@hotmail.com>
2023-06-08 11:02:34 -0500

    correction pycodestyle

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -2,4 +2,4 @@
 def square_matrix_simple(matrix=[]):
     if not matrix:
         return
-    return  [list(map(lambda num: num ** 2, i)) for i in matrix]
+    return [list(map(lambda num: num ** 2, i)) for i in matrix]
