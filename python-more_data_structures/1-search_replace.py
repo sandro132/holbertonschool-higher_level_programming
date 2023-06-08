1f9bbbff51952a28e733a5f1d0560a4f91b36fcf
sandro132 <mianveo@hotmail.com>
2023-06-08 13:52:10 -0500

    remove first validation

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    if not my_list:
-        return
-    else:
-        replace_my_list = []
-        for i in my_list:
-            if i == search:
-                replace_my_list.append(replace)
-            else:
-                replace_my_list.append(i)
-        return replace_my_list
+    replace_my_list = []
+    for i in my_list:
+        if i == search:
+            replace_my_list.append(replace)
+        else:
+            replace_my_list.append(i)
+    return replace_my_list
