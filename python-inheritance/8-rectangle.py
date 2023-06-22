6dc58cb897160b7a2cce5f76f6b23fb7abb1438e
sandro132 <mianveo@hotmail.com>
2023-06-22 16:23:39 -0500

    add function that validate

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -11,5 +11,7 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 class Rectangle(BaseGeometry):
     '''Class that inherits from BaseGeometry'''
     def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
         self.__width = width
         self.__height = height
