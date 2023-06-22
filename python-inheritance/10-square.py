9746e0f85b5f8236243bdcddc194e9573d9657f1
sandro132 <mianveo@hotmail.com>
2023-06-22 17:00:57 -0500

    :globe_with_meridians: documentation

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -7,6 +7,7 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
+    '''Class Square inherits from Rectangle'''
     def __init__(self, size):
         self.__size = size
         self.integer_validator("size", size)
