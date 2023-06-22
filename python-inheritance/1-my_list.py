65972d8667d1b709c6fd989203e08d16a1957c64
sandro132 <mianveo@hotmail.com>
2023-06-22 11:58:48 -0500

    add new test

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
-'''Function that source to list'''
+"""
+Function that source to list
+"""
 
 
 class MyList(list):
-    '''class of the list '''
+    """
+    class of the list
+    """
+
     def print_sorted(self):
-        '''print the list sorted'''
+        """
+        print the list sorted
+        """
         n_list = sorted(self).copy()
         print(n_list)
