87424a9f4bf03755a92dda46ecc163e9ecd87857
sandro132 <mianveo@hotmail.com>
2023-06-23 13:43:51 -0500

    :seedling: add function that return obj of the json

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -5,7 +5,7 @@
 import json
 
 
-def to_json_string(my_obj):
+def from_json_string(my_obj):
     '''Return object of the json'''
-    j_obj = json.dumps(my_obj)
+    j_obj = json.loads(my_obj)
     return j_obj
