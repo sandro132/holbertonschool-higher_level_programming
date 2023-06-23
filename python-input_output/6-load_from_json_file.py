8b8efc064830e1be08cbfa08aea8d8ec02dec867
sandro132 <mianveo@hotmail.com>
2023-06-23 14:01:54 -0500

    :monocle_face: add function that create

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''Function that create an object from json'''
+
+
+import json
+
+
+def load_from_json_file(filename):
+    '''open tthe file and creation'''
+    with open(filename, 'x', decoding='utf-8') as files:
+        return json.load(files)
+    
