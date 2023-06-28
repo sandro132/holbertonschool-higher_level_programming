7b59f3eaef7897a63efe80b02377c038f9840f03
sandro132 <mianveo@hotmail.com>
2023-06-28 13:42:50 -0500

    :truck: change format

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -85,5 +85,5 @@ class Rectangle(Base):
 
     def __str__(self):
         '''overriding the __str__'''
-        return f"[Rectangle] ({self.id})\
-            {self.x}/{self.y} - {self.width}/{self.height}"
+        return "[Rectangle] ({}) {}/{} - {}/{}"\
+            .format(self.id, self.x, self.y, self.width, self.height)
