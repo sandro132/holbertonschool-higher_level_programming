3761fd41deded2413878b84d506e148129355ca9
sandro132 <mianveo@hotmail.com>
2023-06-29 11:55:15 -0500

    :necktie: add same method to dictionary to square

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -43,3 +43,12 @@ class Square(Rectangle):
         else:
             for key, value in kwargs.items():
                 setattr(self, key, value)
+
+    def to_dictionary(self):
+        '''return dictionary'''
+        return {
+            'id': self.id,
+            'size': self.size,
+            'x': self.x,
+            'y': self.y
+        }
