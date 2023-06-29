5f0aa1b6bfdd80d76d8cf019993262a9cd5e72ba
sandro132 <mianveo@hotmail.com>
2023-06-29 12:47:32 -0500

    :test_tube: add new method json to base

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+'''import'''
+
+
+import json
+
+
 '''The basis of all'''
 
 
@@ -12,3 +18,11 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    '''decorator static method'''
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        '''return Json string'''
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
