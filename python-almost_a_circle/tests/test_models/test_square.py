bb1f5552d93535b2d7b6c950938a20aed65ba3c2
sandro132 <mianveo@hotmail.com>
2023-06-29 16:15:12 -0500

    szs

--- python-almost_a_circle/tests/test_models/test_square.py
+++ python-almost_a_circle/tests/test_models/test_square.py
@@ -8,14 +8,6 @@ from models.square import Square
 
 class TestBase(unittest.TestCase):
 
-    def test_base_id_generation(self):
-        base1 = Base()
-        base2 = Base()
-        base3 = Base(89)
-        self.assertEqual(base1.id, 1)
-        self.assertEqual(base2.id, 2)
-        self.assertEqual(base3.id, 89)
-
     def test_base_to_json_string_none(self):
         json_string = Base.to_json_string(None)
         self.assertEqual(json_string, "[]")
