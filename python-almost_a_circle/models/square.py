6fa68db196ef093b4266ef99ab7c30043a70deb9
sandro132 <mianveo@hotmail.com>
2023-06-29 11:12:00 -0500

    :technologist: add method update

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -28,3 +28,18 @@ class Square(Rectangle):
         '''overloading the __str__'''
         return '[Square] ({}) {}/{} - {}'\
             .format(self.id, self.x, self.y, self.size)
+
+    def update(self, *args, **kwargs):
+        '''Method that assign an argument to each'''
+        if args:
+            if len(args) == 1:
+                self.id = args[0]
+            if len(args) == 2:
+                self.size = args[1]
+            if len(args) == 3:
+                self.x = args[2]
+            if len(args) == 4:
+                self.y = args[3]
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
