c5e81eabbac8f8f55523558e01faf41fb4a7851c
sandro132 <mianveo@hotmail.com>
2023-06-29 10:29:25 -0500

    :technologist: update method update with **kwargs

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -88,15 +88,19 @@ class Rectangle(Base):
         return "[Rectangle] ({}) {}/{} - {}/{}"\
             .format(self.id, self.x, self.y, self.width, self.height)
 
-    def update(self, *args):
+    def update(self, *args, **kwargs):
         '''Method that assign an argument to each'''
-        if len(args) == 1:
-            self.id = args[0]
-        if len(args) == 2:
-            self.width = args[1]
-        if len(args) == 3:
-            self.height = args[2]
-        if len(args) == 4:
-            self.x = args[3]
-        if len(args) == 5:
-            self.y = args[4]
+        if args:
+            if len(args) == 1:
+                self.id = args[0]
+            if len(args) == 2:
+                self.width = args[1]
+            if len(args) == 3:
+                self.height = args[2]
+            if len(args) == 4:
+                self.x = args[3]
+            if len(args) == 5:
+                self.y = args[4]
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
